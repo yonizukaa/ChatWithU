56e55676edd098bea6ef25eda8956825802855ee
Johan Marin <johanmarin@MacBook-Air-de-Johan.local>
2023-11-29 16:54:51 +0100

    changement

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -4,6 +4,6 @@ class Constants {
   static String appId = "1:634480644405:web:5549258ea987afaefb7c7c";
   static String apiKey = "AIzaSyADZ6Uq-pFQIWMdkGeh7JIozEY7jJM6r_I";
   static String messagingSenderId = "634480644405";
-  static String projectId = "chatappflutter-13318";
+  static String projectId = "ChatWithU";
   final primaryColor = const Color(0xff8643f3);
 }
